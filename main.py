ca949c1fe9a69ba19dff6f00da3abf0a9c0bc68f
GilbrantRoychan <gilbrant.roychan123@gmail.com>
2024-10-22 13:22:41 +0700

    memperbaiki directory

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from website import create_app
+from flask import Flask
+
+#import Module views
+from views.view import home 
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY']  = 'sayMayName'
+    
+    app.register_blueprint(home, url_prefix = '/')
+    return app
+
 
 if __name__ == "__main__":
     
