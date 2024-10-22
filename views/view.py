fcdf43c9e73b04ca9d8b6e977829607cfd50250d
GilbrantRoychan <gilbrant.roychan123@gmail.com>
2024-10-23 01:14:33 +0700

    style:change transition in btn

--- views/view.py
+++ views/view.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template
+from markupsafe import escape
 
 home = Blueprint('home',__name__)
 
@@ -6,4 +7,4 @@ home = Blueprint('home',__name__)
 @home.route('/')
 def index():
     linkwa = 'https://api.whatsapp.com/send/?phone=62089504455462&text&type=phone_number&app_absent=0'
-    return render_template('base.html',wa = linkwa )
+    return render_template('base.html',wa = escape(linkwa) )
